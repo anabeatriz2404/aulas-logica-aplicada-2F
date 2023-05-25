5c83c2f1a04c860c28cb174bbbca4b62ddde9839
anabeatriz2404 <ana2404alcantara@gmail.com>
2023-05-25 07:53:39 -0300

    Retomada de conteúdo de Orientação a Objetos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Aula de Orientação a Objetos
+
+        // Declarar objetos
+        Pessoa adao;
+
+        // Instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+
+        // Definição do formato
+        adao.nome = "Adão";
+        adao.sobrenome = "Silva";
+        eva.nome = "Eva";
+        eva.sobrenome = "Silva";
+
+
+        // Acionar comportamento
+        adao.falar();
+        eva.falar();
+
+        // Criar mais objetos
+        Pessoa qualquer = new Pessoa();
+
+        // Declarar objeto
+        Pessoa rainha;
+
+        // Instanciação do objeto
+        rainha = new Pessoa();
+
+        // Definindo forma da pessoa
+        rainha.nome = "Tina";
+        rainha.sobrenome = "Tunner";
+
+        // Definição de comportamento
+        System.out.println(rainha.falar("Alto"));
+
+
+
+
+
+
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,16 @@
+public class Pessoa {
+
+    // Atributos da classe
+    String nome;
+    String sobrenome;
+
+
+    // Método da classe
+     public void falar() {
+         System.out.println("Falei");
+     }
+     public String falar(String volume){
+         return "Falei " + volume;
+     }
+
+}
